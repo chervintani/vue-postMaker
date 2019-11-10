b3e0ee73fd509a1ec712ad7cf87bbe8f02b5400f
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-11-10 19:12:51 +0800

    to heroku

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+const app = express()
+const http = require('http').Server(app);
+const port = process.env.PORT || 8080
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+
+// app.listen(port)
+// console.log(`app is listening on port: ${port}`)
+
+http.listen(port, '0.0.0.0', function () {
+	console.log('listening on port ' + port);
+});
